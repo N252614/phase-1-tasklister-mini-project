e32f48c156afdbb9715af4cbd5d6e596f1332012
Your Full Name <your-email@example.com>
2025-06-05 12:24:21 -0600

    Finish Tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const task = taskInput.value;
+    buildToDo(task);
+    form.reset();
+  });
 });
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+  const taskList = document.getElementById("tasks");
+  taskList.appendChild(li);
+}
